f1236cbb3b72f9aaaf177c0c0e8aa399e1d49d98
dnzambuli <damunzandm@outlook.com>
2023-05-02 17:58:56 +0300

    string functions

--- Lesson 3/string.cpp
+++ Lesson 3/string.cpp
@@ -4,6 +4,20 @@ int main(){
     std::string phrase = "The quick brown fox jumped over the zebra grazing";
 
     // sellecting by index
-    std::cout << phrase[0];
+    std::cout << phrase[0] << "\n";
+    // accessing q
+    std::cout << phrase[4] << "\n";
+    // modify q to Q
+    phrase[4] = 'Q';
+    std::cout << phrase << "\n";
+
+    /*
+    Using the find method 
+    string.find("string to be found", xy)
+    xy - index to start from
+    */
+   std::cout << phrase.find("brown", 4);
+
+
     return 0;
 }
