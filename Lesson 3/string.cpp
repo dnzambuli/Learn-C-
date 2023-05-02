572113644a4786d2de92bc036bc662b0fc019ed8
dnzambuli <damunzandm@outlook.com>
2023-05-02 17:40:14 +0300

    string indexingh

---
+++ Lesson 3/string.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+    std::string phrase = "The quick brown fox jumped over the zebra grazing";
+
+    // sellecting by index
+    std::cout << phrase[0];
+    return 0;
+}
