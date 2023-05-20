464cb103a770d0f159d83506313a5453269a3109
dnzambuli <damunzandm@outlook.com>
2023-05-20 17:47:54 +0300

    generated error: assignment of read-only variable

--- lesson 4/const.cpp
+++ lesson 4/const.cpp
@@ -3,9 +3,10 @@
 int main(){
     // const keyword specifies the variable value can not be changed
     // ensures variables are only read only 
-    const double pi = 3.14159;
+    const double PI = 3.14159;
+    PI = 2.187;
     double radius = 10;
-    double circumference = 2 * pi * radius;
+    double circumference = 2 * PI * radius;
     
     std::cout << circumference << " cm";
 
